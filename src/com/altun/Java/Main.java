961e845e5d14d3dd811c02a408f55740138321ac
Hatice-Altun <van.altun@outlook.com>
2022-12-24 16:34:06 +0100

    For Loop

---
+++ src/com/altun/Java/Main.java
@@ -0,0 +1,16 @@
+package com.altun.Java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write in Java a simple for loop to display numbers 1 to 10.
+
+        System.out.println("Display integers!");
+
+        for (int index = 0; index < 10; index++) {
+            int number = index + 1;
+            System.out.println("number: " + number);
+
+        }
+    }
+}
